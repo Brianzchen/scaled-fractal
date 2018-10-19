8c071db203b459360a769d4d6c7afa56123cc2e4
Brian Chen <brian.chen@tabcorp.com.au>
2018-10-19 15:39:50 +1100

    add enzyme

--- testUtils/configure.js
+++ testUtils/configure.js
@@ -1,3 +1,7 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { StyleSheetTestUtils } from 'aphrodite';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 StyleSheetTestUtils.suppressStyleInjection();
