68b9eec1c599d1e4f49dbb7f39740869414ccf07
Brian Chen <brian.chen@tabcorp.com.au>
2019-07-07 13:16:21 +1000

    update deps

--- frame/index.js
+++ frame/index.js
@@ -21,50 +21,32 @@ const middleware = [
   routerMiddleware(history),
 ];
 
-// Function to start the rendering of the GUI
-const startApp = () => {
-  const composeEnhancers = (
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ // eslint-disable-line no-underscore-dangle
-    || compose
+const composeEnhancers = (
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ // eslint-disable-line no-underscore-dangle
+  || compose
+);
+
+const store = createStore(
+  reducers(history),
+  composeEnhancers(
+    applyMiddleware(...middleware),
+  ),
+);
+
+const Main = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+);
+
+const domNode = document.getElementById(rootDomTag);
+if (domNode) {
+  ReactDOM.render(
+    <Main />,
+    domNode,
   );
-
-  const store = createStore(
-    reducers(history),
-    composeEnhancers(
-      applyMiddleware(...middleware),
-    ),
-  );
-
-  const Main = () => (
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <App />
-      </ConnectedRouter>
-    </Provider>
-  );
-
-  const domNode = document.getElementById(rootDomTag);
-  if (domNode) {
-    ReactDOM.render(
-      <Main />,
-      domNode,
-    );
-  } else {
-    console.error(`Element id ${rootDomTag} could not be found`);
-  }
-};
-
-// Check if the process is production mode and if it is not
-// do not include redux logger into the build
-if (process.env.NODE_ENV !== 'production') {
-  import(/* webpackChunkName: "logger" */ 'redux-logger').then((module) => {
-    middleware.push(
-      module.createLogger({
-        collapsed: true,
-      }),
-    );
-    startApp();
-  });
 } else {
-  startApp();
+  console.error(`Element id ${rootDomTag} could not be found`);
 }
