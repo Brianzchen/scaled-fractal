3aad4557fa7800a6208819de432a64d91d4608f6
Brian Chen <brian.chen@tabcorp.com.au>
2020-11-21 10:00:30 +1100

    update webpack packages

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const common = require('../webpack.config');
@@ -8,12 +8,13 @@ module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   performance: {
-    hints: 'error',
+    hints: 'warning',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
   },
   plugins: [
-    new TerserPlugin({
-      sourceMap: true,
-    }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
